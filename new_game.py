26515a75e974b1497a816f2fe4030e0bd9a28d94
PhantomTheNightwing <alastair_moynihan@icloud.com>
2023-10-17 16:39:52 -0700

    Add player motion

--- new_game.py
+++ new_game.py
@@ -7,14 +7,15 @@ pygame.display.set_caption('dog game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png').convert()
-ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My Game', False, 'Black')
+sky_surf = pygame.image.load('graphics/Sky.png').convert()
+ground_surf = pygame.image.load('graphics/ground.png').convert()
+text_surf = test_font.render('My Game', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600, 300))
 
-player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect =player_surf.get_rect(midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -22,13 +23,14 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (300,50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos, 250))
-    player_rect.left += 1
+    screen.blit(sky_surf, (0,0))
+    screen.blit(ground_surf, (0,300))
+    screen.blit(text_surf, (300,50))
+
+    snail_rect.x -= 4
+    screen.blit(snail_surf,snail_rect)
+    if snail_rect.x < 100: snail_rect.x = 800
+
     screen.blit(player_surf,player_rect)
 
     pygame.display.update()
